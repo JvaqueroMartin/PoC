f8884ea3bbfa16ecf179ce8fcdae6d04103939a8
JvaqueroMartin <72016378+JvaqueroMartin@users.noreply.github.com>
2020-10-13 18:34:12 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,13 +7,3 @@ variable "location" {
   type        = string
   description = "(Required) Specifies the supported Azure location where the Resource Group exists. Changing this forces a new resource to be created."
 }
-
-variable "cost_center" {
-  type        = string
-  description = "(Required)This tag will report the cost center of the resource and need to be applied to every resource in the subscription"
-}
-
-variable "product" {
-  type        = string
-  description = "(Required) The product tag will indicate the product to which the associated resource belongs to" 
-}
